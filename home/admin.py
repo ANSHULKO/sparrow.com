236b4b3cf3e6d81e7fb9bd8b523b782f45a96b56
ANSHUMAN DWIVEDI <DUBEYANSHU555@GMAIL.COM>
2022-03-12 17:06:35 +0530

    Initial commit

---
+++ home/admin.py
@@ -0,0 +1,15 @@
+import atexit
+
+from django.contrib import admin
+from .models import*
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('procode','title','brand','added_on',)
+    ordering = ('brand',)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','is_published','created_on')
---
+++ home/models.py
@@ -0,0 +1,17 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    procode = models.CharField(max_length=64)
+    title = models.CharField(max_length=50)
+    brand = models.CharField(max_length=20)
+    added_on = models.DateTimeField()
+
+class Article(models.Model):
+    title=models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to ="articles")
+    content = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_on =models.DateTimeField(auto_now=True)
+    
